83119d615f7996bf93cbf2a533103e80e3a1aadd
zeping <zepinglai@gmail.com>
2016-05-27 11:58:38 +0800

    获取http请求响应头信息

--- tools/get_http_header_info.py
+++ tools/get_http_header_info.py
@@ -1,65 +1,34 @@
+
 #!/usr/bin/env python
 #encoding:utf8
 #author: zeping lia
 
 import sys
-import pycurl
-import re
+import httplib
 
-try:
-    from io import BytesIO
-except ImportError:
-    from StringIO import StringIO as BytesIO
+def get_headers_info(url):
+    '''
+    获取url的http头信息
+    :param url:
+    :return:
+    '''
 
-headers = {}
-
-def header_function(header_line):
-    # HTTP standard specifies that headers are encoded in iso-8859-1.
-    # On Python 2, decoding step can be skipped.
-    # On Python 3, decoding step is required.
-    header_line = header_line.decode('iso-8859-1')
-
-    # Header lines include the first status line (HTTP/1.x ...).
-    # We are going to ignore all lines that don't have a colon in them.
-    # This will botch headers that are split on multiple lines...
-    if ':' not in header_line:
-        return
-
-    # Break the header line into header name and value.
-    name, value = header_line.split(':', 1)
-
-    # Remove whitespace that may be present.
-    # Header lines include the trailing newline, and there may be whitespace
-    # around the colon.
-    name = name.strip()
-    value = value.strip()
-
-    # Header names are case insensitive.
-    # Lowercase name here.
-    name = name.lower()
-
-    # Now we can actually record th
-    headers[name] = value
+    conn = httplib.HTTPSConnection(url)
+    conn.request("HEAD","/")
+    res = conn.getresponse()
+    headers =  res.getheaders()
+    return headers
 
 
 if __name__ == "__main__":
 
     try:
         url = sys.argv[1]
-        #url = "http://www.linuxhub.cn"
-        buffer = BytesIO()
-        c = pycurl.Curl()
-        c.setopt(c.URL,url)
-        c.setopt(c.WRITEFUNCTION, buffer.write)
-        # Set our header function.
-        c.setopt(c.HEADERFUNCTION, header_function)
-        c.perform()
-        c.close()
-
+        # url = "http://www.linuxhub.cn"
+        headers = get_headers_info(url)
         print ""
-        # print headers
-        for d, x in headers.items():
-            print d + ": " + x
+        for head in headers:
+            print head[0] + ": " + head[1]
         print ""
 
     except:
