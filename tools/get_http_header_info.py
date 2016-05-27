db031def27f83108f96dd3bc51d3a78b23709c72
zeping <zepinglai@gmail.com>
2016-05-27 14:19:05 +0800

    获取http请求响应头信息

--- tools/get_http_header_info.py
+++ tools/get_http_header_info.py
@@ -1,36 +1,27 @@
-
 #!/usr/bin/env python
 #encoding:utf8
-#author: zeping lia
+#author: zeping lai
 
 import sys
-import httplib
+import httplib2
 
-def get_headers_info(url):
-    '''
-    获取url的http头信息
-    :param url:
-    :return:
-    '''
-
-    conn = httplib.HTTPSConnection(url)
-    conn.request("HEAD","/")
-    res = conn.getresponse()
-    headers =  res.getheaders()
-    return headers
-
+def get_http_resp_headers(url):
+    h = httplib2.Http(".cache")
+    resp_headers, content = h.request(url, "GET")
+    return resp_headers
 
 if __name__ == "__main__":
 
     try:
         url = sys.argv[1]
         # url = "http://www.linuxhub.cn"
-        headers = get_headers_info(url)
+        resp_headers = get_http_resp_headers(url)
         print ""
-        for head in headers:
-            print head[0] + ": " + head[1]
+        print "URL: " + url
+        for d, x in resp_headers.items():
+            print d + ": " + x
         print ""
-
+        
     except:
         print "\n请在脚本后面输入url地址!  "
         print "例: %s http://www.linuxhub.org\n" % sys.argv[0]
